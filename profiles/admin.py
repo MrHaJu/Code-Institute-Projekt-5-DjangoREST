112ae513c1d53887748d33640fbd4ccdefedc16f
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-12-13 11:46:59 +0100

    update admin.py with Posts and ingredients

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Profile
+from posts.models import Post, Ingredient
 
 admin.site.register(Profile)
+class IngredientInline(admin.TabularInline):
+    model = Post.ingredients.through
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [IngredientInline]
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Ingredient)
