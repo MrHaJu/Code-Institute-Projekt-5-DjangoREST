fa05615b75b89aa6c1b1c2fcf9bfca101bdca7f2
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-12-19 15:07:01 +0100

    preparing to deploy

--- frontend/food-app/src/api/axiosDefaults.js
+++ frontend/food-app/src/api/axiosDefaults.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-//axios.defaults.baseURL = 'https://recipe-share-pp5-ea6d44d9d1e0.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.baseURL = '/api';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
