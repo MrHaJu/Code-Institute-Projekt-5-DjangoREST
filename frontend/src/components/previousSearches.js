cd984b3b6dbdca10215407e2af11d9b9990ee676
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2024-01-01 15:49:10 +0100

    add search function to posts

--- frontend/src/components/previousSearches.js
+++ frontend/src/components/previousSearches.js
@@ -1,5 +1,4 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faSearch } from "@fortawesome/free-solid-svg-icons"
+
 
 export default function PreviousSearches() {
     const searches = [
@@ -17,17 +16,13 @@ export default function PreviousSearches() {
     return (
         <div>
             <div className="previous-searches section">
-                <h2>Previous Searches</h2>
+                <h2>Popular Searches</h2>
                 <div className="previous-searches-container">
                     {searches.map((search, index) => (<div key={index} style={{animationDelay: index *.1 + "s" }} className="search-item">
                         {search}
                     </div>))}
                 </div>
-                <div className="search-box">
-                    <input type="text" placeholder="search ..." />
-                    <button className="btn"><FontAwesomeIcon icon={faSearch} /></button>
-                </div>
-                </div>
+            </div>
         </div>
     )
 }
--- frontend/src/pages/Recipes.js
+++ frontend/src/pages/Recipes.js
@@ -2,7 +2,7 @@
 import PreviousSearches from "../components/previousSearches";
 import React, { useEffect, useState } from "react";
 import Asset from "../components/Asset";
-//import Form from "react-bootstrap/Form";
+import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
@@ -10,7 +10,10 @@ import { useLocation } from "react-router-dom";
 import { axiosReq } from "../api/axiosDefaults";
 import Post from "./auth/posts/Post";
 import NoResults from "../assets/no-results.png";
-
+import {
+  faSearch
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 //import appStyles from "../../App.module.css";
 //import styles from "../../styles/PostsPage.module.css";
@@ -20,11 +23,12 @@ function Recipes({ message, filter = "" }) {
     const [posts, setPosts] = useState({ results: [] });
     const [hasLoaded, setHasLoaded] = useState(false);
     const { pathname } = useLocation();
-  
+    const [query, setQuery] = useState("");
+
     useEffect(() => {
       const fetchPosts = async () => {
         try {
-          const { data } = await axiosReq.get(`/posts/?${filter}`);
+          const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
           setPosts(data);
           setHasLoaded(true);
         } catch (err) {
@@ -34,15 +38,20 @@ function Recipes({ message, filter = "" }) {
   
       setHasLoaded(false);
       fetchPosts();
-    }, [filter, pathname]);
+    }, [filter, query, pathname]);
 
     return (
         <main>
-            <PreviousSearches />
+            <PreviousSearches onSubmit={(event) => event.preventDefault()} onChange={(event) => setQuery(event.target.value)}/>
             <div className="recipes-container">
               <Row className="h-100">
                 <Col className="py-2 p-0 p-lg-2" lg={8}>
-                  <p>Popular profiles mobile</p>
+                  <div className="search-box">
+                  <Form onSubmit={(event) => event.preventDefault()}>
+                    <Form.Control value={query} onChange={(event) => setQuery(event.target.value)}  type="text" placeholder="Search..."/>
+                  </Form>
+                  <button  className='btn'><FontAwesomeIcon icon={faSearch} /></button>
+                  </div>
                   {hasLoaded ? (
                     <>
                       {posts.results.length ? (
@@ -62,7 +71,7 @@ function Recipes({ message, filter = "" }) {
                   )}
                 </Col>
                 <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-                  <p>Popular profiles for desktop</p>
+
                 </Col>
               </Row>
             </div>
