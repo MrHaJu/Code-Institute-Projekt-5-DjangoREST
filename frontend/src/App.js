f885c01a45eed56f6b78b82825325edf642d911b
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2024-01-02 14:05:15 +0100

    deleted commented out code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,14 +7,13 @@ import Recepies from "./pages/Recipes";
 import Settings from "./pages/Settings";
 import Register from "./pages/auth/Register";
 import Login from "./pages/auth/Login";
-//import Logout from "./pages/auth/Logout";
 import Footer from "./components/Footer";
 import PostCreateForm from "./pages/auth/posts/PostCreateForm"
 import './api/axiosDefaults'
 import { createContext, useEffect, useState } from "react";
 import axios from "axios";
 import PostPage from "./pages/auth/posts/PostPage"
-//import { useCurrentUser } from "./contexts/CurrentUserContext";
+
 export const currentUserContext = createContext()
 export const setCurrentUserContext = createContext()
 
@@ -24,7 +23,7 @@ function App() {
     null
   )
   
-  //const currentUser = useCurrentUser();
+  
   const profile_id = currentUser?.profile_id || "";
 
   const handleMount = async () => {
@@ -49,14 +48,12 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/recipes" element={<Recepies message="No results found. Adjust the search keyword."  />} />
-          <Route exact path="/recipes" element={<Recepies message="No results found. Adjust the search Keyword or follow a user." filter={`owner__followed__owner__profile=${profile_id}&`} />} />
           <Route exact path="/liked" element={<Recepies message="No results found. Adjust the search Keyword or like a post." filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`} />} />
           <Route exact path="/bookmarked-posts" element={<Recepies message="No results found. Bookmark a post." filter={`isBookmarked=${currentUser?.profile_id || ""}&`} />} />
-          <Route exact path="/posts/:id" render={<PostPage />} />
+          <Route exact path="/recipes/:id" render={<PostPage />} />
           <Route exact path="/settings" element={<Settings />} />
           <Route exact path="/register" element={<Register />} />
           <Route exact path="/login" element={<Login />} />
-          {/*<Route exact path="/logout" element={<Logout />} />*/}
           <Route exact path="/post/create" element={<PostCreateForm />} />
            {/* Fallback for unknown Routes */}
           <Route path="*" element={<NotFound />} />
